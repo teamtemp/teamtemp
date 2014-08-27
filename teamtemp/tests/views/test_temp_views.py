809b029b78810e665075aa6c88d88b84903b230c
Travis Warren <travisjwarren@gmail.com>
2014-08-28 07:07:33 +1000

    Removing collect static from ProcFile.  Not normally the place I would expect it as heroku deployment runs this command.
    Bumped up to django 1.6 to take advantage of newer test runner.
    
    Adding test suite for views, still queries around submit view and it's URL composition.
    
    Project could now be added to Travis CI if interested.

--- teamtemp/tests/views/test_temp_views.py
+++ teamtemp/tests/views/test_temp_views.py
@@ -1,8 +1,13 @@
 __author__ = 'traviswarren'
 
+from datetime import datetime
+
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from responses.models import TeamTemperature
+
+from teamtemp import utils
 
 
 class TempViewTestCases(TestCase):
@@ -10,26 +15,12 @@ class TempViewTestCases(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='user', email='test@email.com', password='password')
 
-    def test_temp_redirects_if_not_authenticated(self):
-
-        response = self.client.get(reverse('temp'), 'asdfghjk')
-
-        self.assertRedirects(response, 'http://testserver')
-
-    # def test_admin_get_creation(self):
+    # def test_temp_redirects_if_not_authenticated(self):
     #
-    #     self.assertTrue(self.client.login(username=self.user.username, password='password'))
+    #     temp = TeamTemperature.objects.get_or_create(id=utils.random_string(8),
+    #                                                  creation_date=datetime.now(), creator=self.user)
     #
-    #     response = self.client.get(reverse('admin'))
+    #     # think this view could move to a Class based view using kwargs and uuid's to clean up testing
+    #     response = self.client.get(temp[0].id)
     #
-    #     self.assertTemplateUsed(response, 'admin.html')
-    #     self.assertContains(response, 'Create a Team Temperature Survey', status_code=200)
-    #
-    # def test_admin_post_creation(self):
-    #
-    #     self.assertTrue(self.client.login(username=self.user.username, password='password'))
-    #
-    #     response = self.client.post(reverse('admin'))
-    #
-    #     # having form id created in post makes testing correct redirect difficult
-    #     self.assertContains(response, '', status_code=302)
+    #     self.assertRedirects(response, 'http://testserver')
