413cd043a36e96b950f21caced53c85e26be7da6
Travis Warren <travisjwarren@gmail.com>
2014-10-03 07:05:13 +1000

    Moving result from function based view to class based view.
    Adding new mix in for authorisation checks.  This is to allow a more flexible approach to authorisation in the future.
    
    Also update template to use context object for data.  UX improvement to warn user if no result have yet been recorded against a temp.
    
    Adding coveralls for a published view of code coverage pushed out form build.
    
    Adding coverall coverage badge for quick coverage and link to metrics on coverall.

--- teamtemp/urls.py
+++ teamtemp/urls.py
@@ -3,14 +3,14 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.conf import settings
 
-from teamtemp.views import submit, register, result, CreateTeamTemperatureView
+from teamtemp.views import submit, register, TeamTemperatureDetailView, CreateTeamTemperatureView
 
 urlpatterns = patterns(
     '',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^about$', TemplateView.as_view(template_name='about.html'),
         name='about'),
-    url(r'^admin/([0-9a-zA-Z]{8})$', login_required(result), name='result'),
+    url(r'^admin/(?P<pk>[0-9a-zA-Z]{8})/$', login_required(TeamTemperatureDetailView.as_view()), name='result'),
     url(r'^admin/$', login_required(CreateTeamTemperatureView.as_view()), name='admin'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^accounts/register/$', register, name='register'),
